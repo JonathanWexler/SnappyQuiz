3a0049fcbb154d8220b08a2a4a1314feede519a5
Jonathan Wexler <Jwexman@gmail.com>
2013-04-25 21:23:10 -0400

    small change

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,11 @@
 class CommentsController < ApplicationController
    http_basic_authenticate_with :name => "dhh", :password => "secret", :only => :destroy
  
-  def create
+   def create
      @post = Post.find(params[:post_id])
      @comment = @post.comments.create(params[:comment])
      redirect_to post_path(@post)
-   end
+    end
    
    def destroy
       @post = Post.find(params[:post_id])
